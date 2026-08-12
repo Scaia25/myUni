b7bbd2b438a29dcef792b0d726bedd14b04fbd96
Scaia25 <simonescainelli@gmail.com>
2026-08-12 23:51:22 +0200

    struttura del db

--- backend/db.sql
+++ backend/db.sql
@@ -3,5 +3,7 @@ CREATE DATABASE myuni;
 USE myuni;
 
 CREATE TABLE utenti (
-    
-)
+    email varchar(255) primary key,
+    password varchar(255) not null,
+    budget decimal(6,2) default 500
+);
